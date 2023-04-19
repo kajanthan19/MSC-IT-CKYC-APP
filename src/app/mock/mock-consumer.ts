ca7feb3e87d15b3c2b2f70b71e6db05b3c483c99
Kajanthan Kathirgamanathan <kajanthan2001@gmail.com>
2023-04-19 22:39:39 +0100

    updated the new changes

--- src/app/mock/mock-consumer.ts
+++ src/app/mock/mock-consumer.ts
@@ -2,13 +2,25 @@ import { Consumer } from '../models/Consumer'
 
 export const CONSUMERDATA: Consumer[] = [
   {
-    id: 11,
-    FullName: 'Content 1',
-    TypeId: 'string',
-    ConsumerNumber: 'string',
-    ConsumerAddress: 'string',
-    ConsumerOccupation: 'string',
-    ConsumerMobileNo: 'string',
-    Description: 'string'
+    ID : 40,
+    consumer_name: 'string',
+    consumer_type : 'string',
+    registered_date : 'string',
+    consumer_ref : 'string',
+    fee_template : 'string',
+    is_add_cust_allowed : 'string',
+    Encryption_type : 'string',
+    Product_Code : 'string',
+    Created_on : 'string',
+    Created_by : 'string',
+    Modified_on: 'string',
+    Modified_by : 'string',
+    Is_active : 'string',
+    CreatedAt : 'string',
+    cosumer_group_id : 'string',
+    ClientPublicKeyPath : 'string',
+    ClientPrivateKeyPath : 'string',
+    ServerPublicKeyPath : 'string',
+    ServerPrivateKeyPath : 'string',
   }
 ]
--- src/app/models/Consumer.ts
+++ src/app/models/Consumer.ts
@@ -1,10 +1,22 @@
-export class Consumer {
-    id!: number;
-    FullName!: string;
-    TypeId!: string;
-    ConsumerNumber!: string;
-    ConsumerAddress!: string;
-    ConsumerOccupation!: string;
-    ConsumerMobileNo!: string;
-    Description!: string;
+export interface Consumer {
+    ID :number;
+    consumer_name: string;
+    consumer_type : string;
+    registered_date : string;
+    consumer_ref : string;
+    fee_template : string;
+    is_add_cust_allowed : string;
+    Encryption_type : string;
+    Product_Code : string;
+    Created_on : string;
+    Created_by : string;
+    Modified_on: string;
+    Modified_by : string;
+    Is_active : string;
+    CreatedAt : string;
+    cosumer_group_id : string;
+    ClientPublicKeyPath : string;
+    ClientPrivateKeyPath : string;
+    ServerPublicKeyPath : string;
+    ServerPrivateKeyPath : string;
 }
